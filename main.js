6705abdd18c6c4eab6751658b281d181553b5626
aopoku6 <56067572+aopoku6@users.noreply.github.com>
2020-09-22 17:28:14 +0000

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
 
+const navBtn = document.querySelector(".btn-toggle");
+
+const sidebar = document.querySelector(".sidebar");
+
+navBtn.addEventListener("click", () => {
+    sidebar.classList.toggle("show-sidebar")
+})
